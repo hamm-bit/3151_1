27ecf231f0fd5e0970654943b03f46a0b77160e6
hamm-bit <namiicoo@yahoo.com>
2023-07-16 23:07:07 +1000

    cleaned up array and node

--- BucketArray.java
+++ BucketArray.java
@@ -57,52 +57,35 @@ public class BucketArray {
 
         // POTENTIAL ISSUE: not mutex with cleanup()
 
-
-        
-        // Queue starts here
-        // TODO: implement semaphore for holding the read and write pointers in order
         numWrite++;
 
         while (size > INIT_SIZE);
         // surrender insertion and wait for vacancy in array (delete()).
         readLock.lock();
         try {
-                // if (size > INIT_SIZE) {readLock.unlock();}
-                // else {readLock.lock(); break;}
-             
-            
-            System.out.println("insert: passing to BucketNode");
+            // System.out.println("insert: passing to BucketNode");
 
             bucket.insert(item);
             size++;
         } finally {
             numWrite--;
             readLock.unlock();
-            System.out.println("insert: readlock unlocked");
+            // System.out.println("insert: readlock unlocked");
         }
-
-        
-        System.out.println("insert: proceeding to cleanup");
-
+        // System.out.println("insert: proceeding to cleanup");
         cleanup();
         // reset the deletion counter
     }
 
 
     public void appendInsert(Integer item) {
-        System.out.println("appendInsert: passOn detected on final array element");
-
         // wait for all existing inserts to finish, and buffer the appending element
         numAppend++;
-
         // Priority queue.add is O(N)
         itemBuffer.add(item);
-        
-        System.out.println("appendInsert: number of append thread detected, buffering item");
-
+        // System.out.println("appendInsert: number of append thread detected, buffering item");
         // while (numWrite > numAppend || numSearch > 0) /*appendNew.awaitUninterruptibly()*/ ;
-
-        System.out.println("appendInsert: spinlock cleared");
+        // System.out.println("appendInsert: spinlock cleared");
 
         /*
          * NEW discovery
@@ -111,22 +94,13 @@ public class BucketArray {
          */
 
         // writeLock.lock();
-
-        System.out.println("appendInsert: writelock engaged");
+        // System.out.println("appendInsert: writelock engaged");
  
         // determine how many buckets are needed
         int bucketCount = itemBuffer.size() / 4 + 1;
-        // ArrayList<BucketNode> newArray = new ArrayList<>();
         // append each element to have linear complexity
         for (int i = 0; i < bucketCount; i++) {
             BucketNode newNode = new BucketNode(this);
-            
-            /**
-             * TODO: (this problem has been resolved)
-             * This is a priority queue, no worries
-             */
-
-            // gradually fill all the nodes and append them in-order
             while (!itemBuffer.isEmpty() && !newNode.isFullAndTail()) {
                 newNode.insert(itemBuffer.poll());
             }
@@ -149,58 +123,30 @@ public class BucketArray {
         deletedCount += bucket.delete(item) == true ? 1 : 0;
 
         if (deletedCount >= 6) cleanup();
-            /*
-             * NOTE: new idea HAS BEEN IMPLEMENTED
-             * utilize the search function's latest updates to perform a one-dim nearest neighbour search
-             * since each search would yield local space
-             * This does mean that each cleanup() would also have to update the index of each bucket.
-             */
-            
-
-            // updateNearest should also have a binary semaphore
-            // buckets must not vanish when deleting
-            // new buckets can only be appended so it wouldn't affect the correctness
-            // TODO: implement a lock for bucket deletion
-            // TODO: implement an update function that stays sensitive to the
     }
     
 
     public void cleanup() {
         // TODO: a semaphore for one cleanup() concurrent at a time
-        /**
-         * TODO:
-         * also implement a lock which all pointers that queues the cleanup method
-         * cleanup() should wait for all searches to finish
-         * and blocks all following search until cleanup has completed
-         */
-
-        // All buckets after the cleanup should have nearestLeft = nearestRight = 0
-
-        System.out.println("cleanup: attempting to acquire writelock");
-
+        // System.out.println("cleanup: attempting to acquire writelock");
         writeLock.lock();
-
-        System.out.println("cleanup: writelock engaged");
+        // System.out.println("cleanup: writelock engaged");
 
         try {
             // FIFO delete op
             delSem.acquireUninterruptibly();
-
-            System.out.println("cleanup: mutex acquired");
+            // System.out.println("cleanup: mutex acquired");
 
             // signal all incoming search process to surrender
             janitorCount = 1;
     
             // wait until current searches finish
-
-            
-
             while (numWrite != 0 || numSearch != 0) {
-                System.out.printf("cleanup: waiting for numWrite: %d == 0, numSearch %d == 0", numWrite, numSearch);
+                // System.out.printf("cleanup: waiting for numWrite: %d == 0, numSearch %d == 0", numWrite, numSearch);
                 afterHour.awaitUninterruptibly();
             }
-            
-            // TODO: cleanup() main body
+
+            // cleanup() main body
             for (int i = numBuckets - 2; i >= 0; i--) {
                 // start with second last to avoid accidentally removing concurrent appendInsert() nodes
                 BucketNode curr = buckets.get(i), prevNext = buckets.get(i + 1);
@@ -237,15 +183,15 @@ public class BucketArray {
     private BucketNode sparseSearch(int l, int r, Integer item) {
         // Not an approprioate terminating condition
         if (r - l<= 1) {
-            System.out.println("search terminated: no match");
+            // System.out.println("search terminated: no match");
             return buckets.get(r).head() > item ? buckets.get(l) : buckets.get(r);
         }
 
         int m = (l + r) / 2;
-        System.out.printf("sparseSearch: m = %d\n", m);
+        // System.out.printf("sparseSearch: m = %d\n", m);
         BucketNode curr = buckets.get(m);
         if (curr.count == 0) {
-            System.out.println("sparseSearch: empty node detected =========");
+            // System.out.println("sparseSearch: empty node detected =========");
             int left = m - 1, right = m + 1;
             while (true) {
                 // NOTE: this can break
@@ -265,7 +211,7 @@ public class BucketArray {
             }
         }
 
-        System.out.println("sparseSearch: SEARCHING ==============================");
+        // System.out.println("sparseSearch: SEARCHING ==============================");
 
         curr = buckets.get(m);
         if (curr.head() == item) return curr;       
@@ -283,16 +229,13 @@ public class BucketArray {
     private BucketNode search(int item) {
         BucketNode bucket = null;
         // sparse binary search
+            // if cleanup() is queued, block all incoming reads
+            // if new buckets need to be created, also block all incoming reads
         while (janitorCount > 0 || numAppend > 0);
         readLock.lock();
         try {
-            // if cleanup() is queued, block all incoming reads
-            // if new buckets need to be created, also block all incoming reads
 
             int l = 0, r = numBuckets - 1; 
-            System.out.printf("search: prepping binary search, l = %d, r = %d\n", l, r);
-            // while (numWrite != 0) inSearch.awaitUninterruptibly();
-
             /* NOTE:
             * Since new buckets can only be appended, this should have no effect on the r index
             * of the binary search and should remain in-bound
@@ -301,15 +244,11 @@ public class BucketArray {
             numSearch++;
             // here readlock acquires / ensure acquires
             bucket = sparseSearch(l, r, item);
-            System.out.printf("search: obtained bucket head: %d\n", bucket.head());
+            // System.out.printf("search: obtained bucket head: %d\n", bucket.head());
             numSearch--;
         } finally {
             readLock.unlock();
         }
-        
-        // TODO: condition inSearch will turn off when all await has finished
-        // IMPLEMENT A QUEUE FOR READS, they will ALL read concurrently as soon as vacancy negates.
-
         // if (numSearch == 0) inSearch.signalAll();
         
         return bucket;
@@ -322,20 +261,13 @@ public class BucketArray {
     }
 
     public void print_sorted() {
-        // This should be a special read state, which would wait for all regular read and writes to finish
-        // then iterate through the printer
-
-        System.out.printf("print_sorted: number of buckets: %d\n", numBuckets);
+        // System.out.printf("print_sorted: number of buckets: %d\n", numBuckets);
 
         readLock.lock();
         try {
-            // while(numWrite != 0) inPrint.awaitUninterruptibly();
-            // first test maximized concurrency, if concurrency is enough
-            // add (attempted) linear waiting
-
             buckets.stream().forEach(item -> item.print());
+            System.out.println();
         } finally {
-            // inPrint.signalAll();
             readLock.unlock();
         }
         // finish
--- BucketNode.java
+++ BucketNode.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import java.util.concurrent.locks.*;
 import java.util.concurrent.Semaphore;
-import java.util.concurrent.BlockingQueue;
 
 public class BucketNode {
     public int count = 0;
@@ -16,11 +15,8 @@ public class BucketNode {
     private LinkedList<Integer> bucket;
     private ReadWriteLock lock = new ReentrantReadWriteLock();
     private Lock readLock = lock.readLock(), writeLock = lock.writeLock();
-    private Semaphore WSem = new Semaphore(1);
-    // DSem = new Semaphore(1);
-    // private Semaphore RSem = new Semaphore(1);
-    
-    // private BlockingQueue<Integer> q = new LinkedBlockingDeque<>();
+    // private Semaphore WSem = new Semaphore(1);
+
     public BucketNode next = null;
     public BucketArray higher;
     
@@ -42,44 +38,21 @@ public class BucketNode {
         bucket.add(idx, item);
     }
 
-    
-    // /**
-    //  * Pass-on insert, falls under same shared condition and queue write
-    //  * only erectable by other BucketNodes
-    //  */
-    // public void passOnInsert(Integer item) {
-    //     try {
-    //         // TODO: change this to a semaphore/listening monitor
-    //         writeLock.lock();
-
-    //     } finally {
-
-    //     }
-    // }
-
-
     public void insert(Integer item) {
         // TODO: change this to a semaphore/listening monitor
         try {
             writeLock.lock();
-
             // if consecutive writes, kept lock until no more consecutive
             /*
              * first_item = q.pop();
              *  
              *  do {
              */
-        
-            System.out.println("node_insert: writelock engaged");
-
+            // System.out.println("node_insert: writelock engaged");
             // WSem.acquireUninterruptibly();
-
-            System.out.println("node_insert: mutex acquired");
-
+            // System.out.println("node_insert: mutex acquired");
             if (count == 0) {
-
-                System.out.println("node_insert: case empty bucket");
-
+                // System.out.println("node_insert: case empty bucket");
                 bucket = new LinkedList<Integer>();
                 bucket.add(item);
 
@@ -91,22 +64,13 @@ public class BucketNode {
                 passOn(bucket.removeLast());
                 count--;
             }
-
-
-            System.out.println("node_insert: node insertion complete");
-
-
+            // System.out.println("node_insert: node insertion complete");
             count++;
-            // if consecutive writes, kept lock until no more consecutive
-            // } while (q.peekFirst().isWrite());
-
         } finally {
             // WSem.release();
             writeLock.unlock();
         }
-        
-        System.out.println("node_insert: writelock Unlocked");
-
+        // System.out.println("node_insert: writelock Unlocked");
     }
 
     public boolean lookUp(Integer item) {
@@ -116,13 +80,9 @@ public class BucketNode {
         boolean found = false;
         // unlimited concurrency
         try {
-            System.out.println("node_lookUp: lock acquired");
-            System.out.println(head());
+            // System.out.println("node_lookUp: lock acquired");
+            // System.out.println(head());
             found = bucket.contains(item);
-            // for (int i = 0; i < count; i++) {
-            //     System.out.println("nonde_lookUp: searching");
-            //     if (bucket.get(i).equals(item)) found = true;
-            // }
         } finally {
             readLock.unlock();
         }
@@ -134,38 +94,23 @@ public class BucketNode {
         writeLock.lock();
         // mutex sem
         try {
-            System.out.println("node_delete: lock acquired");
-            // remove the item in a traditional fashion
-            // for some reason LL.remove behaves bizarrely for non-existing element
+            // System.out.println("node_delete: lock acquired");
             if (bucket.remove( (Integer) item )) {
-                System.out.println("node_delete: item removed");
+                // System.out.println("node_delete: item removed");
                 count--;
-            } else {
-                System.out.println("node_delete: item not present");
-            }
-
+            } 
+            // else {
+            //     // System.out.println("node_delete: item not present");
+            // }
         } finally {
             writeLock.unlock();
         }
         return count == 0;
     }
 
-    // public Integer insertHead(Integer item) {
-    //     if (count < 4) {
-    //         bucket.addFirst(item);
-    //     } else {
-    //         Integer tail = bucket.removeLast();
-    //         // insert the tail node at the next node
-    //         return passOn(tail);
-    //     }
-    // }
-
     public void passOn(Integer item) {
         // queue this as a write to the next bucket
         if (next == null) {
-            // signal main function to insert
-            // this has the advantage of no need to create a new bucket,
-            // since one may have already been created during this call.
             higher.appendInsert(item);
             return;
         }
@@ -178,10 +123,10 @@ public class BucketNode {
 
     public void print() {
         readLock.lock();
-        System.out.println("bucket: ");
+        // System.out.println("bucket: ");
         try {
             bucket.stream().forEach(item -> System.out.printf("%d ",item));
-            System.out.println();
+            // System.out.println();
         } finally {
             readLock.unlock();
         }
